eca25beb054f7f3a8caa4ef20d65c2f47e0ae299
ilhamgurbanly10 <Ilham_gurbanly@mail.ru>
2023-09-13 17:21:33 +0500

    c172113

--- src/components/footer/FooterComponent.vue
+++ src/components/footer/FooterComponent.vue
@@ -12,12 +12,20 @@ import {
 
 <template>
 
-    <footer class="main-container page-top-spacing">
+    <footer class="main-container">
 
-        
+        <div class="bg-img flex relative flex-col items-center justify-end" :class="$style.box" :style="{ backgroundImage: 'url(https://grandrestaurantv6-7.b-cdn.net/wp-content/uploads/2022/09/footer-pattern.png)'}">
+
+            <h3 class="text-white pb-10 mb-10">© 2023 By Ilham Gurbanly, All Rights Reserved</h3>
+            
+            <div :class="$style.innerBox" class="absolute p-10 w-full h-full text-white">
+                
+            </div>
+
+        </div>
         
     </footer>
 
 </template>
 
-<style src="@/assets/styles/components/Footer.scss" lang="scss" scoped />
+<style src="@/assets/styles/components/Footer.scss" lang="scss" scoped module />
